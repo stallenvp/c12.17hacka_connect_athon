c3d98ff862141210bf720a44c78c13bc1b12d166
Khaleel Younis <khaleelyounis1@gmail.com>
2018-01-03 15:04:24 -0800

    added click handler function to dynamically create token/ added css

--- script.js
+++ script.js
@@ -1,15 +1,42 @@
 $(document).ready(initializeApp);
 
 function initializeApp() {
-    $('.col').click(getId);
+    $('#0').click(clickHandler);
+    $('#1').click(clickHandler);
+    $('#2').click(clickHandler);
+    $('#3').click(clickHandler);
+    $('#4').click(clickHandler);
+    $('#5').click(clickHandler);
+    $('#6').click(clickHandler);
 }
 
+// click handler functions
 
 function getId() {
     var idOfColumn = $(this).attr('id');
     console.log(idOfColumn);
     return idOfColumn;
 }
+
+
+function coinCreation(col,bottom) {
+    var token = $('<div>').addClass('token');
+    $(token).css('bottom', bottom);
+    var img = $('<img>').attr('src','images/trx.png');
+    img.appendTo(token);
+    token.appendTo(col);
+}
+
+function clickHandler() {
+    var bottom = 0.1;
+    var col = $(this);
+    coinCreation(col,bottom);
+}
+
+
+
+
+// game board logic functions
 
 var board = [
     [0, 0, 0, 0, 0, 0, 0],
@@ -30,7 +57,6 @@ function playerToggle(){
         player = 1;
     }
 }
-
 function updateBoard(colValue){
     for(var r = board.length-1; r>=0; r-- ){
         if(board[r][colValue]===0){
@@ -39,6 +65,5 @@ function updateBoard(colValue){
             return;
         }
     }
-
 }
 
